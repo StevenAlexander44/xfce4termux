a24b68351937781dd44534537818bcac68e2e82d
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-01-21 11:37:13 -0500

    Update xfce4termux.sh

--- xfce4termux.sh
+++ xfce4termux.sh
@@ -48,3 +48,6 @@ echo -e 'Desktop Commands:\n * desktop - xfce4 vnc session\n * novnc - access vn
 git clone https://github.com/StevenAlexander44/xfce4termux
 mv xfce4termux/config ~/.config
 rm -rf xfce4termux
+echo;echo;echo Download is Comlete
+echo Recommended install:
+echo; echo pkg i tree neofetch cpufetch openssh rsync mpv-x
