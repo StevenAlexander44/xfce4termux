8087e564d08512389d7e24ecb9259720901a36cb
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-02-02 22:56:19 -0500

    Update xfce4termux.sh

--- xfce4termux.sh
+++ xfce4termux.sh
@@ -7,22 +7,8 @@ mkdir .vnc
 echo -e '#!/data/data/com.termux/files/usr/bin/sh\nxfce4-session &\nxhost + &' > .vnc/xstartup
 chmod u+x .vnc/xstartup
 
-echo -e 'rm -rf $TMPDIR/.X*\nvncserver :0 -listen tcp -geometry 1280x720' > $PATH/desktop
-chmod u+x $PATH/desktop
-echo -e 'vncserver -kill :0' > $PATH/desktopstop
-chmod u+x $PATH/desktopstop
-echo -e 'if [ $(Xvnc -c) = 0 ]\nthen\n desktop\nelse\n desktopstop\nfi' > $PATH/desktoptoggle
-chmod u+x $PATH/desktoptoggle
-
 # noVNC
-git clone https://github.com/novnc/noVNC .novnc
-
-echo -e 'sed -i "s/busy/available/g" `grep -lr "novnctoggle" .config/xfce4/panel`\n~/.novnc/utils/novnc_proxy' > $PATH/novnc
-chmod u+x $PATH/novnc
-echo -e 'pkill -f novnc_proxy\nsed -i "s/available/busy/g" `grep -lr "novnctoggle" .config/xfce4/panel`' > $PATH/novncstop
-chmod u+x $PATH/novncstop
-echo -e 'if [ $(pgrep -f novnc_proxy -c) = 0 ]\nthen\n novnc\nelse\n novncstop\nfi' > $PATH/novnctoggle
-chmod u+x $PATH/novnctoggle
+git clone https://github.com/novnc/noVNC $PREFIX/opt/novnc
 
 # alpine and chromium
 proot-distro install alpine
@@ -30,27 +16,27 @@ proot-distro login alpine -- apk update
 proot-distro login alpine -- apk upgrade
 proot-distro login alpine -- apk add chromium gtk+3.0 tzdata
 proot-distro login alpine -- cp /usr/share/zoneinfo/America/New_York /etc/localtime
-echo -e 'proot-distro login alpine -- sh -c "export DISPLAY=:0 && export PULSE_SERVER=127.0.0.1 && chromium-browser --no-sandbox"' > $PATH/chromium
-chmod u+x $PATH/chromium
-
-# audio
-echo -e 'pulseaudio --start --exit-idle-time=-1\npacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1\nsed -i "s/muted/high/g" `grep -lr "audiotoggle" .config/xfce4/panel`' > $PATH/audio
-chmod u+x $PATH/audio
-echo -e 'pulseaudio -k\nsed -i "s/high/muted/g" `grep -lr "audiotoggle" .config/xfce4/panel`' > $PATH/audiostop
-chmod u+x $PATH/audiostop
-echo -e 'if [ $(pgrep pulseaudio -c) = 0 ]\nthen\n audio\nelse\n audiostop\nfi' > $PATH/audiotoggle
-chmod u+x $PATH/audiotoggle
-
-# motd
-echo -e 'Desktop Commands:\n * desktop - xfce4 vnc session\n * novnc - access vnc in a browser\n * audio - pulseaudio for alpine\n * chromium - chromium in alpine\n' >> ~/../usr/etc/motd
 
 # config
 git clone https://github.com/StevenAlexander44/xfce4termux
 (cd ~/xfce4termux/config && tar c .) | (cd ~/.config && tar xf -)
 rm -rf xfce4termux
 
-echo;echo
-echo Download is Comlete
-echo Recommended install:
-echo
-echo pkg i tree neofetch cpufetch openssh rsync mpv-x htop
+# aliases
+echo -e 'replacePanel() { sed -i "s/$1/g" `grep -lr "$2toggle" .config/xfce4/panel`; }' >> .bashrc
+echo -e 'desktop() { rm -rf $TMPDIR/.X*; vncserver :0 -listen tcp -geometry 1280x720 }' >> .bashrc
+echo -e 'desktopstop() { vncserver -kill :0 }' >> .bashrc
+echo -e 'desktoptoggle() { if [ $(Xvnc -c) = 0 ]; then desktop; else desktopstop; fi }' >> .bashrc
+echo -e 'novnc() { replacePanel busy/available novnc; $PREFIX/novnc/utils/novnc_proxy }' >> .bashrc
+echo -e 'novncstop() { pkill -f novnc_proxy; replacePanel available/busy novnc }' >> .bashrc
+echo -e 'novnctoggle() { if [ $(pgrep novnc_proxy -fc) = 0 ]; then novnc; else novncstop; fi }' >> .bashrc
+echo -e 'chromium() { proot-distro login alpine -- sh -c "export DISPLAY=:0 && export PULSE_SERVER=127.0.0.1 && chromium-browser --no-sandbox" }' >> .bashrc
+echo -e 'audio() { pulseaudio --start --exit-idle-time=-1; pacmd load-module module-native-protocol-tcp auth-ip-acl="127.0.0.1;10.0.0.0/8;192.168.0.0/16" auth-anonymous=1; replacePanel muted/high audio }' >> .bashrc
+echo -e 'audiostop() { pulseaudio -k; replacePanel high/muted audio }' >> .bashrc
+echo -e 'audiotoggle() { if [ $(pgrep pulseaudio -c) = 0 ]; then audio; else audiostop; fi }' >> .bashrc
+
+# motd
+echo -e 'Desktop Shortcuts:\n * desktop - xfce4 vnc session\n * novnc - access vnc in a browser\n * audio - local audio for alpine\n * chromium - chromium in alpine\n' >> $PREFIX/etc/motd
+
+echo Download is complete. Exit and restart termux to complete installation.
+echo Recommended packages: pkg i mpv-x tree htop openssh rsync mpv-x neofetch cpufetch iproute2 man aria2
